22b3d135d8ba2d0fb690ab4b8e630d6181086705
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-09-21 04:21:28 +0300

    fix(installer): install OpenSSH client on Debian

--- scripts/install/distros/debian.sh
+++ scripts/install/distros/debian.sh
@@ -2,7 +2,7 @@
 install_packages(){
   export DEBIAN_FRONTEND=noninteractive
   apt-get update
-  local pkgs=(cockpit nut-client nut-server dialog jq curl ca-certificates openssl)
+  local pkgs=(cockpit nut-client nut-server dialog jq curl ca-certificates openssl openssh-client)
   [[ "$NETWORK_MODE" == restricted ]] && pkgs+=(nftables)
   apt-get install -y --no-install-recommends "${pkgs[@]}"
   if ! command -v go>/dev/null && [[ ! -x "${BINARY_DIR:-}/cockpit-ups-wol-agent" ]]; then apt-get install -y --no-install-recommends golang-go; fi
