410176a6e63a6fb7efe17c24619e2f4ffbddf961
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-09-21 04:35:26 +0300

    chore(installer): remove dead duplicate systemd installer

--- scripts/install/systemd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-install_systemd_units(){
-  install -m0644 "$SELF_DIR/packaging/systemd/cockpit-ups-wol-agent.service" "$SYSTEMD_DIR/"
-  install -m0644 "$SELF_DIR/packaging/systemd/cockpit-ups-wol-health.service" "$SYSTEMD_DIR/"
-  install -m0644 "$SELF_DIR/packaging/systemd/cockpit-ups-wol-health.timer" "$SYSTEMD_DIR/"
-  install -m0644 "$SELF_DIR/packaging/systemd/cockpit-ups-wol-firewall.service" "$SYSTEMD_DIR/"
-}
